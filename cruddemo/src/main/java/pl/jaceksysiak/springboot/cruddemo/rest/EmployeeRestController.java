3d1451a91922c078103953be262a9c84ecbf3370
Jacek <Jacek@BERLIN>
2020-03-19 01:04:00 +0100

    gg

---
+++ cruddemo/src/main/java/pl/jaceksysiak/springboot/cruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,42 @@
+package pl.jaceksysiak.springboot.cruddemo.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import pl.jaceksysiak.springboot.cruddemo.dao.EmployeeDAO;
+import pl.jaceksysiak.springboot.cruddemo.entity.Employee;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+	private EmployeeDAO employeeDAO;
+	
+	//inject emplyoee dao (use constructor injection)
+	@Autowired
+	public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
+		employeeDAO = theEmployeeDAO;
+	}
+	
+	// expose "/employees" and return list of employees
+	@GetMapping("/employees")
+	public List<Employee> findAll() {
+		return employeeDAO.findAll();
+	}
+
+	
+}
+
+
+
+
+
+
+
+
+
+
